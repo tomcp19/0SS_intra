c7477474bb9bb26e755ec6085a0c1a85764b5006
tomcp19 <tomcp19@gmail.com>
2020-02-24 13:00:55 -0500

    No 4 et 5 (Biblio de classe et projet app wpf

---
+++ Wpf_intra/intra_models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace intra_models
+{
+    public class Customer:INotifyPropertyChanged
+    {
+        public string Name;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string Province;
+        public string PostalCode;
+        public string ContactInfo;
+        public string PicturePath;
+
+        public string Info { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
